21f91fc8057ce882a9225506d43079150e1405bc
AbdulRazaque665 <abdulrazaque.xwave@gmail.com>
2024-09-22 19:04:14 +0500

    created components

--- src/App.js
+++ src/App.js
@@ -1,23 +1,15 @@
 import './App.css';
+import Header from './header';
+import Herosection from './herosection';
+import Footer from './footer'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={""} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+   <div>
+    <Header />
+    <Herosection />
+    <Footer />
+   </div>
   );
 }
 
